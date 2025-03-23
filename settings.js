c1a7d75bc6cf203324bee739ae23d2cdc7321798
cliffpaule <162073754+cliffpaule@users.noreply.github.com>
2025-03-23 13:35:26 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Dr Cliff', // Your name
+  ownerNumber: '254716160048', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
